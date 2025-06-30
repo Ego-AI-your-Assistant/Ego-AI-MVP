42e4e619c09766d95e81b13664232a31753859f3
setterwars <s.zaynulin@innopolis.university>
2025-06-30 13:11:09 +0300

    fix cors policy

--- backend/main.py
+++ backend/main.py
@@ -34,7 +34,7 @@ logger.info(f"CORS origins: {cors_origins}")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://egoai.duckdns.org:3000"],
+    allow_origins=['http://egoai.duckdns.org:3000'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
