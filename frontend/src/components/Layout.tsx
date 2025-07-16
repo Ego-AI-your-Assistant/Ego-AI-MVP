6726dcf5fc828998ae771b1a8c443d0aece7c724
setterwars <s.zaynulin@innopolis.university>
2025-07-16 12:31:00 +0300

    little fix

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -25,9 +25,8 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
         <div className="nav-links">
           <Link to="/calendar" className="nav-link">Calendar</Link> {/* Fill with links to pages */}
           <Link to="/chat" className="nav-link">Chat with AI</Link>
-          <Link to="" className="nav-link">Dashboard/Analytics</Link> 
           <Link to="/recs" className="nav-link">Recommendations</Link>
-          <Link to="/geo-search" className='nav-link'>GEO-ASSISTANT</Link>
+          <Link to="/geo-search" className='nav-link'>Geo-recomendations</Link>
           <Link to="/profile" className="nav-link">Profile</Link>
         </div>
         <button onClick={handleLogout} className="logout-btn">
